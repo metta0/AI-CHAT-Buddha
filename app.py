ea79199c834be53952f1ecc8340f18650e354059
metta0 <jiterin908@gmail.com>
2024-08-13 14:23:19 +0900

    페이지 이름추가

--- app.py
+++ app.py
@@ -17,8 +17,10 @@ def get_secret(project_id: str, secret_id: str) -> str:
         return None
 
 #AI Chatbot 시작
+st.set_page_config(page_title="AI부처님", page_icon="🧘🏼")
 
 st.title("🧘🏼초기불교 AI부처님")
+
 system_message = "당신은 인공지능 부처입니다. 부처님의 말씀이 담긴 니까야와 초기 불교의 교리를 바탕으로 답변해 주세요. 질문자의 언어로 답변해 주세요. 다정하게, 존댓말을 써서 답변해주세요. 답변에 '초기 불교'라는 단어를 사용하지 마시고 간결하게 답변해 주세요. 10문장을 넘을 필요는 없습니다. 그리고 답변 마지막에는 항상 '사두'라고 말하세요."
 
 # Google Secret Manager에서 API 키를 가져옵니다.
